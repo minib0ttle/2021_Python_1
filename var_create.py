43621034d5ae282b2dcbad95b1c46f089c436e29
kal501 <69739898+kal501@users.noreply.github.com>
2021-06-26 16:55:47 +0900

    Update var_create.py

--- var_create.py
+++ var_create.py
@@ -1,3 +1,28 @@
 pi = 3.141592265
 
-pi
+print(pi)
+
+#변수에 저장된 값으로 연산하기
+
+print(pi+2)
+print(pi-2)
+print(pi*2)
+print(pi/2)
+print(pi//2)
+print(pi*pi)
+print(pi%2)
+
+#pi를 이용한 원의 둘레와 넓이 구하기
+#파이썬은 변수를 사용할 때 
+#변수의 자료형을 선언하지 않음
+#그렇기 때문에 유연성이 생겨
+#같은 변수에 여러가지 자료형을 넣을 수 있음!
+#TypeError 발생할 수 있다
+
+r = 10
+
+print("원주율은 =",pi)
+print("반지름 r = ",r)
+print("원의 둘레 =",2*r*pi)
+print("원의 넓이 =",r*r*pi)
+
