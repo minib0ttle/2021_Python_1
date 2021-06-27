1fdd558d57756b93afe5caff246703c9f476f57a
kal501 <69739898+kal501@users.noreply.github.com>
2021-06-27 14:37:52 +0900

    Create list_oper3.py

---
+++ list_oper3.py
@@ -0,0 +1,24 @@
+# 리스트 내부에 있는지 확인하기
+# in/not in 연산자
+
+list_a = [273, 32, 103, 57, 52]
+273 in list_a
+#True
+
+99 in list_a
+#False
+
+100 in list_a
+#False
+
+52 in list_a
+#True
+
+273 not in list_a
+#False
+
+99 not in list_a
+#True
+
+100 not in list_a
+#True
