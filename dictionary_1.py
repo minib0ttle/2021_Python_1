ef6e79c5fb1d5060bb4d9e93d76f4a0a03561d8b
kal501 <69739898+kal501@users.noreply.github.com>
2021-06-27 16:12:36 +0900

    dictionary_example

--- dictionary_1.py
+++ dictionary_1.py
@@ -69,7 +69,7 @@ dictionary = {
     "name" : "7D 건조 망고",
     "type" : "당절임",
     "ingredient" : ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
-    "orgin" : "필리핀"
+    "orgin" : "필리핀",
 }
 
 key = input(" 접근하고싶은 키 :")
