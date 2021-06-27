f48b171eab74550132f71d9953ba58cafcaed1b2
kal501 <69739898+kal501@users.noreply.github.com>
2021-06-27 16:39:55 +0900

    Create fuction_1.py

---
+++ fuction_1.py
@@ -0,0 +1,15 @@
+def print_3_times():
+    print("안녕하세요")
+    print("안녕하세요")
+    print("안녕하세요")
+
+print_3_times()
+
+
+def print_n_times(value, n):
+    for i in range(n):
+        print(value)
+
+
+print_n_times("안녕하세요", 5)
+
